126d7c3c1559d2a9b2517335431b559a777d48ab
The Mohit Soni <mohit@market-pulse.in>
2025-04-29 20:03:17 +0530

    Modified task category - starter

--- shared/src/commonMain/kotlin/com/droidcon/taskzen/models/TaskCategory.kt
+++ shared/src/commonMain/kotlin/com/droidcon/taskzen/models/TaskCategory.kt
@@ -1,11 +1,20 @@
 package com.droidcon.taskzen.models
 
 import androidx.compose.ui.graphics.Color
+import com.droidcon.taskzen.generated.resources.Res
+import com.droidcon.taskzen.generated.resources.design
+import com.droidcon.taskzen.generated.resources.grocery
+import com.droidcon.taskzen.generated.resources.social
+import com.droidcon.taskzen.generated.resources.sport
+import com.droidcon.taskzen.generated.resources.university
+import com.droidcon.taskzen.generated.resources.work
+import org.jetbrains.compose.resources.DrawableResource
 
-enum class TaskCategory(val color: Color) {
-    WORK(Color(0xFF6200EE)),
-    PERSONAL(Color(0xFF03DAC5)),
-    STUDY(Color(0xFFBB86FC)),
-    FITNESS(Color(0xFF3700B3)),
-    URGENT(Color(0xFFFF5722)),
+enum class TaskCategory(val color: Color, val res: DrawableResource) {
+    GROCERY(Color(0xFFCCFF80), Res.drawable.grocery),
+    WORK(Color(0xFFFF9680), Res.drawable.work),
+    SPORT(Color(0xFF80FFFF), Res.drawable.sport),
+    DESIGN(Color(0xFF80FFD9), Res.drawable.design),
+    UNIVERSITY(Color(0xFF809CFF), Res.drawable.university),
+    SOCIAL(Color(0xFFFF80EB), Res.drawable.social),
 }
